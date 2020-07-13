061d29e6b9f66ae5dcb3273e894031300b6102ee
Dominic Michaud <dmichaud@fungo.ca>
2020-07-13 14:49:07 -0400

    cleanup project / add bulma / setup multilingual

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Hugo Webpack Demo
+# Hugo Webpack
 
 This repo is a demo on how you can setup webpack with Hugo.
 
@@ -6,5 +6,36 @@ https://dev.to/tegandbiscuits/building-a-static-site-with-hugo-and-webpack-pd3
 
 To run this, you will need to have yarn, and hugo installed.
 
-With a little modification, you can use the webpack dev server instead of the one Hugo comes with.
-[This pull request](https://github.com/tegandbiscuits/hugo-webpack-demo/pull/1/files) shows how to set that up.
+# Install HUGO
+
+---
+
+### Install Hugo
+
+`$ brew install hugo`
+
+### Verify install
+
+`$ which hugo`
+
+### Verify version
+
+`$ hugo version`
+or
+`$ hugo help`
+
+# Local Development
+
+---
+
+### Set correct node version
+
+`nvm install lts/dubnium` (if you don't have it installed)
+
+or
+
+`nvm use lts/dubnium`
+
+### Run Hugo server
+
+`$ brew install hugo`
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,81 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// ./webpack.config.js
 
-module.exports = {
-  entry: [
-    path.resolve('src', 'js', 'app.js'),
-    path.resolve('src', 'styles', 'app.css'),
-  ],
-  output: {
-    path: path.resolve('static', 'assets'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader',
-        ],
-      },
+const path = require('path')
+const autoprefixer = require('autoprefixer')
+const cssnano = require('cssnano')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ManifestPlugin = require('webpack-manifest-plugin')
+
+module.exports = env => {
+  const isProduction = Boolean(env && env.production)
+  console.log('Production: ', isProduction)
+
+  return {
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    entry: path.resolve(__dirname, 'src/js/index.js'),
+    output: {
+      path: path.resolve(__dirname, 'static/'),
+      filename: `js/${isProduction ? '[hash].' : ''}bundle.js`,
+      publicPath: '/'
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: `css/${isProduction ? '[hash].' : ''}bundle.css`
+      }),
+      new ManifestPlugin({
+        fileName: '../data/manifest.json'
+      })
     ],
-  },
-  plugins: [new MiniCssExtractPlugin()],
-};
+    devServer: {
+      port: 1314
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 2,
+                sourceMap: !isProduction
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: !isProduction,
+                plugins: [
+                  cssnano({ preset: 'default' }),
+                  autoprefixer({ grid: true })
+                ]
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: !isProduction
+              }
+            }
+          ]
+        },
+        {
+          test: /\.(png|jpg|gif|svg)$/,
+          loader: 'file-loader',
+          options: {
+            outputPath: 'assets/'
+          }
+        }
+      ]
+    }
+  }
+}
